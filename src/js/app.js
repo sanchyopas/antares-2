6c8626a7d2e2b7587d09dfbe701f104456d80c59
Александр Голованёв <saniagolovanev@gmail.com>
2023-03-29 18:05:51 +0700

    Fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
+console.log("Работает");
+
 import * as functions from "./modules/functions.js";
 
 import './libs/slider.js'
@@ -24,3 +26,29 @@ AOS.init({
     // easing: 'ease-in-quad'
 });
 
+let isMobile = { Android: function () { return navigator.userAgent.match(/Android/i); }, BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i); }, iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i); }, Opera: function () { return navigator.userAgent.match(/Opera Mini/i); }, Windows: function () { return navigator.userAgent.match(/IEMobile/i); }, any: function () { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); } };
+
+if (isMobile.any()) {
+    document.body.classList.add('_touch');
+} else {
+    document.body.classList.add('_pc');
+
+}
+
+
+let itemMenuList = document.querySelectorAll('.menu-bottom__item');
+
+if (itemMenuList.length > 0) {
+    itemMenuList.forEach(item => {
+
+        let child = item.querySelector('.submenu');
+
+        if (child) {
+            item.innerHTML += `
+            <span class="menu-bottom__arrow" style="background-image: url(../img/icons/arrow-down.svg);"></span>
+            `;
+        }
+
+
+    })
+}

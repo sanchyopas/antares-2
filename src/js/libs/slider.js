7cd565f7c8ff97eb3a3307171dad4b1b8d18dc63
Александр Голованёв <saniagolovanev@gmail.com>
2023-03-30 18:00:14 +0700

    Верстка меню и доработка некоторых блоков

--- src/js/libs/slider.js
+++ src/js/libs/slider.js
@@ -3,81 +3,33 @@ import Swiper, { Navigation, Pagination } from 'swiper';
 
 
 // init Swiper:
-const swiper = new Swiper('.swiper', {
-    // Подключаем модули слайдера
-    // для конкретного случая
-    modules: [Navigation],
-    observer: true,
-    observeParents: true,
-    slidesPerView: 3,
-    spaceBetween: 20,
+const swiper = new Swiper('.reviews__slider', {
+    slidesPerView: 2.5,
+    spaceBetween: 8,
+    allowSlideNext: true,
+    allowSlidePrev: true,
     autoHeight: true,
-    speed: 800,
-
-    touchRatio: 0,
-    simulateTouch: false,
-    loop: false,
-    // preloadImages: true,
-    lazy: true,
-
-    /*
-    // Эффекты
-    effect: 'fade',
-    autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-    },
-    */
-
-    // Пагинация
-    /*
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    */
-
-    // Скроллбар
-    /*
-    scrollbar: {
-        el: '.swiper-scrollbar',
-        draggable: true,
-    },
-    */
-
-    // Кнопки "влево/вправо"
-    navigation: {
-        prevEl: '.gallery__btn--prev',
-        nextEl: '.gallery__btn--next',
-    },
-
-    // Брейкпоинты
-    /*
+    observer: true,
     breakpoints: {
-        320: {
+        0: {
             slidesPerView: 1,
-            spaceBetween: 0,
-            autoHeight: true,
+            spaceBetween: 14,
         },
-        768: {
+        580: {
+            slidesPerView: 2
+        },
+        921: {
+            slidesPerView: 1,
+        },
+        1060: {
+            allowTouchMove: true,
             slidesPerView: 2,
-            spaceBetween: 20,
         },
-        992: {
-            slidesPerView: 3,
-            spaceBetween: 20,
-        },
-        1268: {
-            slidesPerView: 4,
-            spaceBetween: 30,
-        },
-    },
-    */
-    // События
-    on: {
-
+        1260: {
+            slidesPerView: 2.5,
+            loop: false,
+        }
     }
-
 });
 const gallery__lider = new Swiper('.gallery__slider', {
     slidesPerView: 3,
@@ -85,6 +37,7 @@ const gallery__lider = new Swiper('.gallery__slider', {
     allowSlideNext: true,
     allowSlidePrev: true,
     autoHeight: true,
+    observer: true,
 
     freeMode: false,
     breakpoints: {
@@ -96,16 +49,16 @@ const gallery__lider = new Swiper('.gallery__slider', {
         },
         767: {
             spaceBetween: 10,
-            // slidesPerView: 'auto',
             allowTouchMove: true,
+            slidesPerView: 2
         },
         1024: {
             allowTouchMove: true,
+            slidesPerView: 3,
         },
         1260: {
-            allowTouchMove: false,
             spaceBetween: 15,
-            slidesPerView: 2,
+
             slidesPerColumn: 2,
             loop: false,
         }
